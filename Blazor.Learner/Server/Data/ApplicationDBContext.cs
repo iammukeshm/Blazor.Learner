1193eddc07d8ccce3e117c4950f40346167b8da6
Mukesh Murugan <iammukeshm@gmail.com>
2020-06-03 22:21:31 +0530

    Add project files.

---
+++ Blazor.Learner/Server/Data/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Blazor.Learner.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.Learner.Server.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options)
+        {
+        }
+        public DbSet<Developer> Developers { get; set; }
+    }
+}
---
+++ Blazor.Learner/Shared/Models/Developer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazor.Learner.Shared.Models
+{
+    public class Developer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public decimal Experience { get; set; }
+    }
+}
